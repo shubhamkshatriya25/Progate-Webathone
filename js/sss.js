ebc6717b311fcc598ea179bec76ab6a360a99dcf
Shubham Kshatriya <shubhamkshatriya521@gmail.com>
2020-02-26 14:31:17 +0530

    Add files via upload

---
+++ js/sss.js
@@ -0,0 +1,14 @@
+
+var count=0;
+function truee(){
+    alert("Correct Answer");
+    count++;
+}
+
+function falsee() {
+    alert("Wrong Answer");
+}
+function checkk() {
+    alert(count);
+    count=0;
+}
